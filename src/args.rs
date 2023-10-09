4bbb540e3aa4d3deeb94ccdbb8de1b6035ffd471
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-10-09 23:18:55 +0800

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,27 +1,33 @@
-pub use clap::Parser;
+pub(crate) use clap::Parser;
 use std::net::SocketAddr;
 use std::process::Command;
 
-pub const CFGTOOL: &str = "on";
+pub(crate) const CFGTOOL: &str = "on";
+
+pub(crate) const SOCKETSAFE: &str = "off";
 
 /// Arguments
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
-pub struct Args {
+pub(crate) struct Args {
     /// The specified socket to receive configuration
     #[arg(short, long, default_value_t = SocketAddr::from(([127, 0, 0, 1], 0)))]
-    pub socket: SocketAddr,
+    pub(crate) socket: SocketAddr,
 
     /// Configuration should be from allowable ips. Scope is "localhost" when it's empty
     #[arg(long)]
-    pub ipscope: Option<Vec<String>>,
+    pub(crate) ipscope: Option<Vec<String>>,
 
     /// Use cfg tool if = "on"
     #[arg(long)]
-    pub cfgtool: Option<String>,
+    pub(crate) cfgtool: Option<String>,
+
+    /// No use safe connection to socket if = "off"
+    #[arg(long)]
+    pub(crate) socsafe: Option<String>,
 }
 
-pub fn use_cfgtool(addr: SocketAddr) {
+pub(crate) fn use_cfgtool(addr: SocketAddr) {
     let _process = match Command::new("./cfgtool")
         .arg("-t")
         .arg(addr.to_string())
