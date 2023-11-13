194503eeca33b6b794ed6a9d9e9d8ec2c931811d
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-11-13 15:50:47 +0800

    Update cfgtool.rs

--- src/bin/cfgtool.rs
+++ src/bin/cfgtool.rs
@@ -1,14 +1,58 @@
 use clap::Parser;
-use std::net::SocketAddr;
-use tokio::io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+use std::fmt::Debug;
+use tokio::io::{
+    self, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader,
+};
 use tokio::net::TcpStream;
+use tokio_native_tls::native_tls::TlsConnector as NativeTlsConnector;
+use tokio_native_tls::TlsConnector;
+
+const END_FLAG: &str = ":!";
+
+const NO: &str = "no";
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
 
-    let mut socket = TcpStream::connect(args.to).await.unwrap();
+    if args.is_safe() {
+        connsafe(&args).await;
+    } else {
+        conn(&args).await;
+    }
+}
 
+async fn connsafe(args: &Args) -> bool {
+    let socket = TcpStream::connect(&args.to).await.unwrap();
+    let c = NativeTlsConnector::new().unwrap();
+    let c = TlsConnector::from(c);
+    let mut s = args.to.to_string();
+    if let Some(n) = args.to.find(':') {
+        s.truncate(n);
+    }
+    match c.connect(s.as_str(), socket).await {
+        Ok(socket) => {
+            input(socket).await;
+            return true;
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return false;
+        }
+    };
+}
+
+async fn conn(args: &Args) -> bool {
+    let socket = TcpStream::connect(&args.to).await.unwrap();
+    input(socket).await;
+    true
+}
+
+async fn input<T>(mut socket: T)
+where
+    T: AsyncRead + AsyncWrite + AsyncWriteExt + Unpin + Debug,
+{
+    println!("{:?}", socket);
     let mut out = io::stdout();
     out.write_all(b"Please input configuration.\n")
         .await
@@ -40,13 +84,21 @@ async fn main() {
     }
 }
 
-const END_FLAG: &str = ":!";
-
 /// Arguments
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// The socket to send configuration
     #[arg(short, long)]
-    pub to: SocketAddr,
+    pub to: String,
+
+    /// Whether to use safe connection to socket (yes/no)
+    #[arg(long, default_value = NO)]
+    socsafe: String,
+}
+
+impl Args {
+    fn is_safe(&self) -> bool {
+        NO != self.socsafe
+    }
 }
