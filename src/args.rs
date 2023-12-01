91930a79f14011cf21310993c6226cff91607b5d
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-01 23:22:04 +0800

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,30 @@
-pub(crate) use clap::Parser;
-use std::process::Command;
+use clap::Parser;
+use lazy_static::lazy_static;
+use log::error;
+use std::process::{Child, Command};
+use std::sync::Mutex;
 
 const YES: &str = "yes";
 
 const NO: &str = "no";
+
+lazy_static! {
+    static ref CHILD: Mutex<Vec<Child>> = Mutex::new(Vec::new());
+}
+
+fn add_child(c: Child) {
+    if let Ok(mut v) = CHILD.lock() {
+        v.push(c);
+    }
+}
+
+fn get_child() -> Option<Child> {
+    if let Ok(mut v) = CHILD.lock() {
+        v.pop()
+    } else {
+        None
+    }
+}
 
 /// Arguments
 #[derive(Parser, Debug, Clone)]
@@ -11,59 +32,75 @@ const NO: &str = "no";
 pub(crate) struct Args {
     /// The specified socket to receive configuration
     #[arg(short, long, default_value = "127.0.0.1:0")]
-    pub(crate) socket: String,
+    socket: String,
 
-    /// Configuration should be from allowable ips. Scope is "localhost" when it's empty
-    #[arg(long)]
-    pub(crate) ipscope: Option<Vec<String>>,
+    /// Configuration should be from allowable ips
+    #[arg(long, default_value = "127.0.0.1,::1")]
+    ipscope: String,
 
     /// Whether to use cfg tool (yes/no)
     #[arg(long, default_value = NO)]
-    pub(crate) cfgtool: String,
+    cfgtool: String,
 
     /// Whether to use safe connection to socket (yes/no)
     #[arg(long, default_value = NO)]
-    pub(crate) socsafe: String,
+    socsafe: String,
 }
 
 impl Args {
+    pub(crate) fn socket(&self) -> &String {
+        &self.socket
+    }
+
     pub(crate) fn is_tool(&self) -> bool {
         YES == self.cfgtool
     }
 
-    pub(crate) fn check_tool(&self, s: String) {
-        if self.is_tool() {
-            if self.is_safe() {
-                std::thread::spawn(move || {
-                    let _process = match Command::new("./cfgtool")
-                        .arg("-t")
-                        .arg(s)
-                        .arg("--socsafe")
-                        .arg(YES)
-                        .spawn()
-                    {
-                        Ok(process) => process,
-                        Err(e) => panic!("fail to spawn 'cfgtool': {}", e),
-                    };
-                });
-            } else {
-                std::thread::spawn(move || {
-                    let _process = match Command::new("./cfgtool")
-                        .arg("-t")
-                        .arg(s)
-                        .arg("--socsafe")
-                        .arg(NO)
-                        .spawn()
-                    {
-                        Ok(process) => process,
-                        Err(e) => panic!("fail to spawn 'cfgtool': {}", e),
-                    };
-                });
-            }
-        }
-    }
-
     pub(crate) fn is_safe(&self) -> bool {
         NO != self.socsafe
+    }
+
+    pub(crate) fn ipscope(&self) -> Vec<String> {
+        self.ipscope.split(',').map(|s| s.to_string()).collect()
+    }
+}
+
+pub(crate) fn init() -> Args {
+    Args::parse()
+}
+
+pub(crate) fn spawn_tool(s: String, safe: bool) {
+    if safe {
+        std::thread::spawn(move || {
+            match Command::new("./cfgtool")
+                .arg("-t")
+                .arg(s)
+                .arg("--socsafe")
+                .arg(YES)
+                .spawn()
+            {
+                Ok(process) => add_child(process),
+                Err(e) => error!("fail to spawn 'cfgtool': {}", e),
+            };
+        });
+    } else {
+        std::thread::spawn(move || {
+            match Command::new("./cfgtool")
+                .arg("-t")
+                .arg(s)
+                .arg("--socsafe")
+                .arg(NO)
+                .spawn()
+            {
+                Ok(process) => add_child(process),
+                Err(e) => error!("fail to spawn 'cfgtool': {}", e),
+            };
+        });
+    }
+}
+
+pub(crate) fn close_tool() {
+    if let Some(mut c) = get_child() {
+        if let Ok(_) = c.kill() {}
     }
 }
