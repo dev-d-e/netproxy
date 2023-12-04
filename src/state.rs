d1ea981c9a095d1f1f309c7ff5f2eeaedaa8a17a
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-04 22:36:34 +0800

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,9 @@
+use crate::core::FuncControl;
+use async_trait::async_trait;
 use lazy_static::lazy_static;
 use log::error;
 use std::collections::HashMap;
-use tokio::sync::oneshot::Sender;
+use tokio::sync::oneshot;
 use tokio::sync::Mutex;
 
 lazy_static! {
@@ -11,8 +13,32 @@ lazy_static! {
 #[derive(Debug)]
 struct ServerState {
     server: String,
-    velocity: f32,
-    tx: Sender<u8>,
+    velocity: u32,
+    tx: oneshot::Sender<u8>,
+}
+
+#[derive(Debug)]
+pub(crate) struct ServerControl {
+    server: String,
+    rx: Vec<oneshot::Receiver<u8>>,
+}
+
+#[async_trait]
+impl FuncControl for ServerControl {
+    fn stop_receiver(&mut self) -> oneshot::Receiver<u8> {
+        if let Some(rx) = self.rx.pop() {
+            rx
+        } else {
+            let (_, rx) = oneshot::channel();
+            rx
+        }
+    }
+
+    async fn velocity(&self, velocity: u32) {
+        if let Some(ss) = SERVER_STATE.lock().await.get_mut(&self.server) {
+            ss.velocity = velocity;
+        }
+    }
 }
 
 async fn add_state(ss: ServerState) {
@@ -23,13 +49,18 @@ async fn remove_state(server: &String) -> Option<ServerState> {
     SERVER_STATE.lock().await.remove(server)
 }
 
-pub(crate) async fn hold(server: String, tx: Sender<u8>) {
+pub(crate) async fn hold(server: String) -> ServerControl {
+    let (tx, rx) = oneshot::channel();
     let ss = ServerState {
-        server,
-        velocity: 0.0,
+        server: server.clone(),
+        velocity: 0,
         tx,
     };
     add_state(ss).await;
+    ServerControl {
+        server,
+        rx: vec![rx],
+    }
 }
 
 pub(crate) async fn list() -> String {
@@ -65,4 +96,15 @@ pub(crate) async fn shutdown(server: &String) -> bool {
         }
     }
     false
+}
+
+pub(crate) async fn no_hold() -> (oneshot::Sender<u8>, ServerControl) {
+    let (tx, rx) = oneshot::channel();
+    (
+        tx,
+        ServerControl {
+            server: String::new(),
+            rx: vec![rx],
+        },
+    )
 }
