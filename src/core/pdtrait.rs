288885421ad6edcd6c28045b69d86a5b475c29b7
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-08-09 15:33:46 +0800

    Update pdtrait.rs

--- src/core/pdtrait.rs
+++ src/core/pdtrait.rs
@@ -1,54 +1,20 @@
-use super::Protoc;
+use super::*;
 use async_trait::async_trait;
 use std::marker::Send;
 
 #[async_trait]
 pub(crate) trait FuncRemote: Clone + Send + Sync + 'static {
-    async fn get(&self, buf: &mut Vec<u8>) -> Option<(Protoc, String, u32)>;
+    async fn get(&mut self, buf: &mut Vec<u8>) -> Option<Remote>;
 }
 
 #[async_trait]
 pub(crate) trait FuncR: Clone + Send + Sync + 'static {
-    async fn data(&self, buf: &mut Vec<u8>);
+    async fn data(&mut self, buf: &mut Vec<u8>);
 
-    async fn enddata(&self, buf: &mut Vec<u8>);
-}
-
-#[macro_export]
-macro_rules! transfer_data {
-    ($name:ident, $self:ident, $buf:ident, $func:stmt, $func2:stmt) => {
-        #[derive(Copy, Clone, Debug)]
-        struct $name;
-
-        #[async_trait]
-        impl FuncR for $name {
-            async fn data(&$self, $buf: &mut Vec<u8>) {
-                $func
-            }
-
-            async fn enddata(&$self, $buf: &mut Vec<u8>){
-                $func2
-            }
-        }
-    };
+    async fn enddata(&mut self, buf: &mut Vec<u8>);
 }
 
 #[async_trait]
 pub(crate) trait FuncRw: Clone + Send + Sync + 'static {
-    async fn service(self, r_buf: &mut Vec<u8>, w_buf: &mut Vec<u8>);
-}
-
-#[macro_export]
-macro_rules! rw_service {
-    ($name:ident, $self:ident, $req:ident, $rsp:ident, $func:stmt) => {
-        #[derive(Copy, Clone, Debug)]
-        struct $name;
-
-        #[async_trait]
-        impl FuncRw for $name {
-            async fn service($self, $req: &mut Vec<u8>, $rsp: &mut Vec<u8>) {
-                $func
-            }
-        }
-    };
+    async fn service(&mut self, r_buf: &mut Vec<u8>, w_buf: &mut Vec<u8>);
 }
