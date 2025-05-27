c346778d97e69d583ccbccc902eff164cea1886c
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-05-27 15:10:47 +0800

    Update hr.rs

--- src/core/hr.rs
+++ src/core/hr.rs
@@ -1,23 +1,34 @@
+use super::into_str;
 use httpenergy::H1RequestUnits;
 use log::trace;
 
-#[derive(Debug)]
 pub(crate) struct HttpRequest<'a> {
     req: H1RequestUnits,
     buf: &'a [u8],
 }
 
 impl<'a> HttpRequest<'a> {
+    ///parse request header
+    pub(crate) fn parse(buf: &'a [u8]) -> Result<Self, String> {
+        trace!("request buf: {:?}", buf.len());
+        let req = H1RequestUnits::new(buf);
+        if req.is_err() {
+            Err("request err".to_string())
+        } else {
+            Ok(Self { req, buf })
+        }
+    }
+
     pub(crate) fn method(&mut self) -> String {
-        self.req.method().to_string()
+        into_str(self.req.method())
     }
 
     pub(crate) fn path(&mut self) -> String {
-        self.req.target().to_string()
+        into_str(self.req.target())
     }
 
     pub(crate) fn version(&mut self) -> String {
-        self.req.version().to_string()
+        into_str(self.req.version())
     }
 
     pub(crate) fn get_header_value(&mut self, str: &str) -> String {
@@ -26,16 +37,5 @@ impl<'a> HttpRequest<'a> {
 
     pub(crate) fn get_host(&mut self) -> String {
         self.get_header_value("Host")
-    }
-}
-
-///parse request header
-pub(crate) fn parse_request<'a>(buf: &'a [u8]) -> Result<HttpRequest, String> {
-    trace!("request buf: {:?}", buf.len());
-    let req = H1RequestUnits::new(buf);
-    if req.is_err() {
-        Err("request err".to_string())
-    } else {
-        Ok(HttpRequest { req, buf })
     }
 }
