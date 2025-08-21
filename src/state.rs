df8721e6f3ad07c8f16fa15e2c7e9e60f2aa2a61
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-08-21 16:03:28 +0800

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,86 +1,42 @@
-use crate::core::{now_str, FuncControl};
-use async_trait::async_trait;
+use crate::core::*;
+use getset::{CopyGetters, Getters, MutGetters, Setters};
 use log::error;
-use std::collections::HashMap;
-use std::net::IpAddr;
-use tokio::sync::{oneshot, Mutex, OnceCell};
+use std::collections::{HashMap, HashSet};
+use std::net::{IpAddr, SocketAddr};
+use tokio::sync::{mpsc, oneshot, Mutex, OnceCell};
 
-static SERVER_STATE: OnceCell<Mutex<HashMap<String, ServerState>>> = OnceCell::const_new();
+static SERVER_STATE: OnceCell<Mutex<HashMap<SocketAddr, ServerState>>> = OnceCell::const_new();
 
-async fn server_state() -> &'static Mutex<HashMap<String, ServerState>> {
+async fn server_state() -> &'static Mutex<HashMap<SocketAddr, ServerState>> {
     SERVER_STATE
         .get_or_init(|| async { Mutex::new(HashMap::new()) })
         .await
 }
 
-#[derive(Debug)]
-struct ServerState {
-    server: String,
-    tx: oneshot::Sender<u8>,
+#[derive(CopyGetters, Getters, MutGetters, Setters)]
+pub(crate) struct ServerState {
+    server: SocketAddr,
+    c: oneshot::Sender<ControlInfo>,
+    #[getset(get_copy = "pub(crate)", set = "pub(crate)")]
     velocity: u32,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
     date_time: String,
+    ip_scope: HashSet<IpAddr>,
 }
 
 impl ServerState {
-    fn new(server: String, tx: oneshot::Sender<u8>) -> Self {
-        ServerState {
+    pub(crate) fn new(server: SocketAddr, c: oneshot::Sender<ControlInfo>) -> Self {
+        Self {
             server,
-            tx,
+            c,
             velocity: 0,
             date_time: String::new(),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub(crate) struct ServerControl {
-    server: String,
-    rx: Option<oneshot::Receiver<u8>>,
-    ip_scope: Vec<String>,
-}
-
-#[async_trait]
-impl FuncControl for ServerControl {
-    fn stop_receiver(&mut self) -> oneshot::Receiver<u8> {
-        if let Some(rx) = self.rx.take() {
-            rx
-        } else {
-            let (_, rx) = oneshot::channel();
-            rx
+            ip_scope: HashSet::new(),
         }
     }
 
-    async fn reject_ip(&self, ip: IpAddr) -> bool {
-        if self.ip_scope.is_empty() {
-            return false;
-        }
-        for s in &self.ip_scope {
-            if s == &ip.to_string() {
-                return false;
-            }
-        }
-        true
-    }
-
-    async fn velocity(&self, velocity: u32) {
-        if let Some(ss) = server_state().await.lock().await.get_mut(&self.server) {
-            ss.velocity = velocity;
-            ss.date_time = now_str();
-        }
-    }
-}
-
-impl ServerControl {
-    pub(crate) fn new(server: String, rx: oneshot::Receiver<u8>) -> Self {
-        ServerControl {
-            server,
-            rx: Some(rx),
-            ip_scope: Vec::new(),
-        }
-    }
-
-    pub(crate) fn get_scope(&mut self) -> &mut Vec<String> {
-        &mut self.ip_scope
+    pub(crate) fn send(self, o: ControlInfo) -> Result<(), ControlInfo> {
+        self.c.send(o)
     }
 }
 
@@ -92,60 +48,88 @@ async fn add_state(ss: ServerState) {
         .insert((&ss.server).clone(), ss);
 }
 
-async fn remove_state(server: &String) -> Option<ServerState> {
+async fn remove_state(server: &SocketAddr) -> Option<ServerState> {
     server_state().await.lock().await.remove(server)
 }
 
-pub(crate) async fn hold(server: String) -> ServerControl {
-    let (tx, rx) = oneshot::channel();
-    add_state(ServerState::new(server.clone(), tx)).await;
-    ServerControl::new(server, rx)
+pub(crate) async fn hold(
+    server: SocketAddr,
+    c: oneshot::Sender<ControlInfo>,
+    mut s: mpsc::Receiver<StateInfo>,
+) {
+    add_state(ServerState::new(server.clone(), c)).await;
+
+    tokio::spawn(async move {
+        while let Some(o) = s.recv().await {
+            match o {
+                StateInfo::Sum(velocity, date_time) => {
+                    if let Some(ss) = server_state().await.lock().await.get_mut(&server) {
+                        ss.set_velocity(velocity);
+                        ss.date_time = date_time;
+                    }
+                }
+            }
+        }
+    });
+}
+
+pub(crate) async fn server_accept(addr: &str, func: impl FuncStream) {
+    if let Ok((mut server, a, b)) = Server::new(addr)
+        .await
+        .inspect_err(|e| error!("new server: {:?}", e))
+    {
+        hold(*server.addr(), a, b).await;
+        server.accept(func).await;
+    }
 }
 
 pub(crate) async fn list() -> String {
     let map = server_state().await.lock().await;
-    let mut str = String::new();
-    str.push_str("ok ");
+    let mut s = String::new();
+    s.push_str("ok ");
     for (key, val) in map.iter() {
-        str.push_str(key);
-        str.push_str(",velocity:");
-        str.push_str(&val.velocity.to_string());
-        str.push_str("c/s");
+        s.push_str(&key.to_string());
+        s.push_str(",velocity:");
+        s.push_str(&val.velocity.to_string());
         if val.date_time.len() > 0 {
-            str.push('[');
-            str.push_str(&val.date_time);
-            str.push(']');
+            s.push(' ');
+            s.push('[');
+            s.push_str(&val.date_time);
+            s.push(']');
         }
-        str.push(' ');
+        s.push(' ');
     }
-    str.pop();
-    str
+    s.pop();
+    s
 }
 
-pub(crate) async fn state_string(server: &String) -> String {
+pub(crate) async fn state_string(server: &SocketAddr) -> String {
     let map = server_state().await.lock().await;
-    let mut str = String::new();
-    str.push_str("ok ");
+    let mut s = String::new();
+    s.push_str("ok ");
     if let Some(ss) = map.get(server) {
-        str.push_str(&ss.server);
-        str.push_str(",velocity:");
-        str.push_str(&ss.velocity.to_string());
+        s.push_str(&ss.server.to_string());
+        s.push_str(",velocity:");
+        s.push_str(&ss.velocity.to_string());
+        if ss.date_time.len() > 0 {
+            s.push(' ');
+            s.push('[');
+            s.push_str(&ss.date_time);
+            s.push(']');
+        }
     }
-    str
+    s
 }
 
 //find signal sender and send stop signal to server
-pub(crate) async fn shutdown(server: &String) -> bool {
-    if let Some(s) = remove_state(server).await {
-        match s.tx.send(0) {
-            Ok(_) => return true,
-            Err(e) => error!("Sender<u8> error: {:?}", e),
-        }
-    }
-    false
-}
-
-pub(crate) async fn no_hold() -> (oneshot::Sender<u8>, ServerControl) {
-    let (tx, rx) = oneshot::channel();
-    (tx, ServerControl::new(String::new(), rx))
+pub(crate) async fn shutdown(server: &SocketAddr) -> bool {
+    remove_state(server)
+        .await
+        .map(|s| {
+            s.send(ControlInfo::Close)
+                .inspect_err(|_| error!("shutdown error"))
+                .ok()
+        })
+        .flatten()
+        .is_some()
 }
