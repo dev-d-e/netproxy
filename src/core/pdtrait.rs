7146588e2b0da96975325d8ea6dd3bd0e83bc83b
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-08-21 15:51:56 +0800

    Update pdtrait.rs

--- src/core/pdtrait.rs
+++ src/core/pdtrait.rs
@@ -1,11 +1,4 @@
-use super::*;
 use async_trait::async_trait;
-use std::marker::Send;
-
-#[async_trait]
-pub(crate) trait FuncRemote: Clone + Send + Sync + 'static {
-    async fn get(&mut self, buf: &mut Vec<u8>) -> Option<Remote>;
-}
 
 #[async_trait]
 pub(crate) trait FuncR: Clone + Send + Sync + 'static {
