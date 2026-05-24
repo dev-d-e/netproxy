a2267c1e7e3a25fbb0616ed5dae5cb3dbc16a665
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2026-05-24 16:14:49 +0800

    Update rt.rs

--- src/core/rt.rs
+++ src/core/rt.rs
@@ -1,4 +1,4 @@
-use log::{debug, error, info};
+use super::*;
 use std::future::Future;
 use std::thread;
 use std::time::Duration;
@@ -8,16 +8,15 @@ pub(crate) fn tokiort_block_on<T>(function: T)
 where
     T: Future,
 {
-    let rt = match Builder::new_multi_thread()
+    let rt = if let Ok(rt) = Builder::new_multi_thread()
         .enable_all()
         .worker_threads(1)
         .build()
+        .map_err(|e| error!("tokiort_block_on: {e}"))
     {
-        Ok(rt) => rt,
-        Err(e) => {
-            error!("Builder error:{:?}", e);
-            return;
-        }
+        rt
+    } else {
+        return;
     };
 
     rt.block_on(function);
@@ -32,12 +31,14 @@ where
 {
     thread::spawn(move || {
         debug!("{:?} new thread", std::thread::current().id());
-        let rt = match Builder::new_multi_thread().enable_all().build() {
-            Ok(rt) => rt,
-            Err(e) => {
-                error!("new thread Builder error:{:?}", e);
-                return;
-            }
+        let rt = if let Ok(rt) = Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .map_err(|e| error!("new_thread_tokiort_block_on: {e}"))
+        {
+            rt
+        } else {
+            return;
         };
 
         rt.block_on(function);
