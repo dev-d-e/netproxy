0b31919e2efe7facaf83288bae3b2b2d17d38b7d
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2026-05-24 16:12:17 +0800

    Update cert.rs

--- src/core/cert.rs
+++ src/core/cert.rs
@@ -1,29 +1,34 @@
 use super::*;
-use std::io::ErrorKind::InvalidData;
-use std::io::{Error, Result};
-use tokio::sync::{Mutex, OnceCell};
+use tokio::sync::{OnceCell, RwLock};
 use tokio_native_tls::native_tls::Identity;
 
-static VALID_IDENTITY: OnceCell<Mutex<Vec<Identity>>> = OnceCell::const_new();
+static VALID_IDENTITY: OnceCell<RwLock<Vec<Identity>>> = OnceCell::const_new();
 
-async fn identity_vec() -> &'static Mutex<Vec<Identity>> {
+async fn identity_vec() -> &'static RwLock<Vec<Identity>> {
     VALID_IDENTITY
-        .get_or_init(|| async { Mutex::new(Vec::new()) })
+        .get_or_init(|| async { RwLock::new(Vec::new()) })
         .await
 }
 
 async fn add_identity(identity: Identity) {
-    identity_vec().await.lock().await.push(identity);
+    identity_vec().await.write().await.push(identity);
 }
 
 async fn get_identity() -> Option<Identity> {
-    identity_vec().await.lock().await.first().cloned()
+    identity_vec().await.read().await.first().cloned()
 }
 
-async fn build_identity(data: &[u8], pwd: &str) -> Result<()> {
-    let i = Identity::from_pkcs12(data, pwd).map_err(|e| Error::new(InvalidData, e))?;
-    add_identity(i).await;
-    Ok(())
+async fn build_identity(data: &[u8], pwd: &str) -> bool {
+    match Identity::from_pkcs12(data, pwd) {
+        Ok(i) => {
+            add_identity(i).await;
+            true
+        }
+        Err(e) => {
+            error!("invalid data: {e}");
+            false
+        }
+    }
 }
 
 ///get certificate info from "VALID_IDENTITY".
@@ -34,14 +39,17 @@ pub(crate) async fn valid_identity() -> Option<Identity> {
 ///input file path and  password
 ///get certificate data from file
 ///then build identity
-pub(crate) async fn build_certificate_from_file(path: String, pwd: String) -> Result<usize> {
-    let f = get_file(&path)?;
-    build_identity(f.as_slice(), &pwd).await.map(|_| f.len())
+pub(crate) async fn build_certificate_from_file(path: String, pwd: String) -> bool {
+    if let Some(f) = get_file(&path) {
+        build_identity(f.as_slice(), &pwd).await
+    } else {
+        false
+    }
 }
 
 ///input socket and  password
 ///get certificate data from socket
 ///then build identity
-pub(crate) async fn build_certificate_from_socket(_soc: String, _pwd: String) -> Result<usize> {
-    Ok(0)
+pub(crate) async fn build_certificate_from_socket(_soc: String, _pwd: String) -> bool {
+    false
 }
